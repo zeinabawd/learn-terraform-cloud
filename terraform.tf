9042fccd879e746b5ae2d6b3a8f2eef8dbe80825
Awad, Zeinab, Vodafone <zeinab.awad-mohamed@vodafone.com>
2022-10-31 23:16:51 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "zeinabawadmohamed"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
